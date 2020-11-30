e1601019adc26ff71496ff7de495e068feed8728
Antoine <antoine.dumont@finelia.fr>
2020-11-30 14:55:47 +0100

    config heroku

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,8 @@
+const dbuser = 'admin';
+const dbpassword = 'Qe1ehZeYjuEnvRWZ';
+//${dbuser}:${dbpassword}
 module.exports = {
-    mongoURI: "mongodb://mongo/project_user",
+    mongoURI: "mongodb+srv://admin:Qe1ehZeYjuEnvRWZ@mern-stack.goyit.mongodb.net/mern-users?retryWrites=true&w=majority",
     secret: "secret",
     email_acc: 'notification.mernstack@gmail.com',
     email_pass: 'jnr2XiRpqG7dknh4Ac7K'
